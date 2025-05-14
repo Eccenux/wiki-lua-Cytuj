eb0b19e97e1cc7ccd7791068144023e73991e96b
eccenux <eccenux+git@gmail.com>
2025-05-14 23:12:25 +0200

    ładowanie pliterkowych i testy

--- test_main.lua
+++ test_main.lua
@@ -10,6 +10,23 @@ function require(moduleName)
 	moduleName = moduleName:gsub("Modu[^:]+:", "")
 	return originalRequire(moduleName)
 end
+-- replace loader for pl-characters replacement
+local originalLoadData = mw.loadData
+local plToAscii = {
+	["ą"] = "a", ["ć"] = "c", ["ę"] = "e", ["ł"] = "l",
+	["ń"] = "n", ["ó"] = "o", ["ś"] = "s", ["ż"] = "z", ["ź"] = "z",
+	["Ą"] = "A", ["Ć"] = "C", ["Ę"] = "E", ["Ł"] = "L",
+	["Ń"] = "N", ["Ó"] = "O", ["Ś"] = "S", ["Ż"] = "Z", ["Ź"] = "Z"
+}
+local function replacePolishChars(str)
+	return (str:gsub("[%z\1-\127\194-\244][\128-\191]*", function(c)
+		return plToAscii[c] or c
+	end))
+end
+function mw.loadData(moduleName)
+	moduleName = replacePolishChars(moduleName)
+	return originalLoadData(moduleName)
+end
 
 -- Load a copy of a module
 -- Note that this loads "ISBN.lua" file (a local file).
@@ -24,3 +41,13 @@ mw.logObject(html)
 local html = p.opis(isbn)
 mw.logObject(html)
 ]==]
+
+local problems = {}
+table.insert(problems, "typ? :: moze za \"duzo\" pol") -- test info z pytajnikiem; opis z cudzysłowem ang.
+table.insert(problems, "pub. albo wyd. :: jednoczesne „opublikowany” oraz „wydawca”") -- test z kropkami
+table.insert(problems, "test as-is: test") -- test bez opisu (np. raport z firewall)
+mw.log(p.__priv.formatProblems(problems):gsub("span>,", "span>,\n"))
+
+local s = "pub--albo--wyd"
+s = s:gsub("%-%-+", "-")
+print(s)
